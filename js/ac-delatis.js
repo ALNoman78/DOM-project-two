6eee4a425682de48570ed782dee350628015f888
ALNoman78 <abdullaalno078@gmail.com>
2024-09-20 09:50:40 +0600

    full form

---
+++ js/ac-delatis.js
@@ -0,0 +1,29 @@
+// step - 1 btn select
+
+document.getElementById('add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const accountNumber = document.getElementById('amount-of-money').value;
+    // console.log(accountNumber);
+
+    const moneyAmount = document.getElementById('amount').value;
+    const moneyNumber = parseFloat(moneyAmount)
+    console.log(moneyNumber);
+
+    const pinNumber = document.getElementById('pin-number').value
+
+    if (pinNumber === '123') {
+        alert("Money Added Successfully");
+
+        const availableBalance = document.getElementById('balance').innerText
+        const balanceNumber = parseFloat(availableBalance)
+        console.log(balanceNumber);
+
+        const updateBalance = balanceNumber + moneyNumber
+        console.log(updateBalance);
+
+        document.getElementById('balance').innerText = updateBalance
+    }else{
+        alert('Error ! input your valid information')
+    }
+})
