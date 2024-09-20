4997762374be814223c8bcd57bf07470823e40f1
ALNoman78 <abdullaalno078@gmail.com>
2024-09-20 12:29:13 +0600

    add balance section added

--- js/normal form.js
+++ js/normal form.js
@@ -12,6 +12,14 @@ document.getElementById('add-money').addEventListener('click',  function (event)
         const availableBalance = getTextInputToNumber('balance');
         const afterAddAmount = availableBalance + amountOfMoney;
         document.getElementById('balance').innerText = afterAddAmount;
+        const div = document.createElement('div')
+        div.innerHTML = `
+            <div class="bg-green-400 p-3 rounded-xl">
+                <h1>Add Balance : </h1>
+                <p>Add money ${amountOfMoney} tk. and after added balance ${afterAddAmount} </p>
+            </div>
+        `
+        document.getElementById('transaction-container').appendChild(div)
     }else{
         alert('Invalid Data ! Check Your account number and pin number')
     }
